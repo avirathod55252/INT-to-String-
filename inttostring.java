288fb3c3cfd2a4af1ad49be7292a6a31548160e2
Avinash Vishnu Rathod <104629418+avirathod55252@users.noreply.github.com>
2022-10-29 17:29:28 +0500

    Create inttostring.java

---
+++ inttostring.java
@@ -0,0 +1,9 @@
+public class inttostring {
+    public static void main(String args[])
+    {
+        int a=5;
+        String s =null;
+        s =Integer.toString(a);
+        System.out.println(s);
+    }
+}
